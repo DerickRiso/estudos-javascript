9121ca9ef5a89a4dae3902f750da8c6d07b646af
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-01-20 10:42:47 -0300

    Novo exercício de condicionais

--- 06 - Estruturas condicionais/condicionais.js
+++ 06 - Estruturas condicionais/condicionais.js
@@ -1,3 +1,14 @@
-console.log("Deu certo?")
-var vel = 100
-console.log(`A velocidade é ${vel}`)
+var entrada = document.getElementById("vel").value
+var vel = Number(entrada)
+var enviar = document.getElementById("enviar")
+enviar.addEventListener('click', calcular)
+
+
+
+function calcular() {
+    if(vel > 60) {
+        document.getElementById("res").innerHTML = `Você passou da velocidade permitida (${vel} Km/h), multado.`
+        alert("OBa")
+        
+    }
+} 
