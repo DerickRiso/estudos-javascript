7806fa0bf02f91cf8fdde36d967ffa95424acc5a
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-01-21 09:15:08 -0300

    bug de escopo resolvido

--- 06 - Estruturas condicionais/6.1/condicionais.js
+++ 06 - Estruturas condicionais/6.1/condicionais.js
@@ -1,14 +1,15 @@
-var entrada = document.getElementById("vel").value
-var vel = Number(entrada)
 var enviar = document.getElementById("enviar")
 enviar.addEventListener('click', calcular)
 
-
-
 function calcular() {
+    var entrada = document.getElementById("vel").value
+    var vel = entrada
+
     if(vel > 60) {
-        document.getElementById("res").innerHTML = `Você passou da velocidade permitida (${vel} Km/h), multado.`
-        alert("OBa")
-        
+        document.getElementById("res").innerHTML = `Você passou da velocidade permitida (${vel} Km/h), multado.`  
+    } else {
+        document.getElementById("res").innerHTML = `Você passou dentro da velocidade permitida (${vel} Km/h)`
     }
+
+    document.getElementById("frase").innerHTML = "Use cinto de segurança"
 } 
