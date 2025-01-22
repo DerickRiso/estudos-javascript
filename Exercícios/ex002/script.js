5dde5dd801c9d4b5588d35511d10f7439a5325d0
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-01-22 11:53:07 -0300

    Exercício concluído

--- Exercícios/ex002/script.js
+++ Exercícios/ex002/script.js
@@ -1,24 +1,54 @@
 function verificar() {
+    // Definição de datas e resultado
     var data = new Date()
     var ano = data.getFullYear() // Ano atual
     var anoNascimento = document.getElementById('txtano') // Ano de nascimento
     var res = document.getElementById("res")
 
+    // Mensagem de erro
     if (anoNascimento.value.length == 0 || anoNascimento.value > ano) {
         alert("Verifique as informações!")
     } else {
+        // Definição de gênero
         var fsex = document.getElementsByName("radsex")
         var idade = ano - Number(anoNascimento.value)
         var genero = ""
+
+        // Criação de uma tag img 
         var img = document.createElement('img')
         img.setAttribute('id', 'foto')
-        
-        if (fsex[0].checked) {
+
+        // Definição das imagens por gênero e idade
+        if (fsex[0].checked) {  // Homem
             genero = "Homem"
-        } else if (fsex[1].checked) {
+            if (idade < 15) {
+                // criança
+                img.setAttribute('src', 'menino.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'homem.png')
+            } else if (idade >= 50) {
+                // idoso
+                img.setAttribute('src', 'idoso.png')
+            }
+        } else if (fsex[1].checked) {   // Mulher
             genero = "Mulher"
+            if (idade < 15) {
+                // criança
+                img.setAttribute('src', 'menina.png')
+            } else if (idade < 50) {
+                // adulto
+                img.setAttribute('src', 'mulher.png')
+            } else if (idade >= 50) {
+                // idoso
+                img.setAttribute('src', 'idosa.png')
+            }
         }
+
+        // Exibição dos resultados
+    
         res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+        res.appendChild(img)
     }
 }
 
