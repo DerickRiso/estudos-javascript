62f7ef7f6d106a9e47ebe03726b18a6c78a9fca8
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-01-24 10:54:15 -0300

    Troquei o método de saída de dados

--- Exercícios/ex003/script.js
+++ Exercícios/ex003/script.js
@@ -1,4 +1,4 @@
-function contagem() {
+function contar() {
     var numInicial = document.getElementById("icomeco")
     var intervalo = document.getElementById("iintervalo")
     var numFinal = document.getElementById("ifinal")
@@ -8,15 +8,17 @@ function contagem() {
     var num1 = Number(numInicial.value) 
     var intrvl = Number(intervalo.value)
     var num2 = Number(numFinal.value)
-    const cont = []
+    // const cont = []
 
-    for(num1; num1 <= num2 ;num1 += intrvl) {
+    for(let i = num1; i <= num2 ;i += intrvl) {
         // função push() posiciona item no array
-        cont.push(num1)
+        // cont.push(num1)
+        res.innerHTML += `\u{1F600}${i}, `
     }
+
 
     // função join() transforma array em string
     // console.log(cont.join(", "))
-
-    res.innerHTML = `<p>${cont.join(", ")}</p>`
+    // res.innerHTML = `<p>${cont.join(", ")}</p>`
+    
 }
