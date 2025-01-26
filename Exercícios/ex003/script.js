a62e8075bfd3ce874cb42894725feda179c1366d
Derick Darlan Riso Santana <derickdarlan304@gmail.com>
2025-01-26 10:01:36 -0300

    Mensagem de erro

--- Exercícios/ex003/script.js
+++ Exercícios/ex003/script.js
@@ -8,17 +8,14 @@ function contar() {
     var num1 = Number(numInicial.value) 
     var intrvl = Number(intervalo.value)
     var num2 = Number(numFinal.value)
-    // const cont = []
 
-    for(let i = num1; i <= num2 ;i += intrvl) {
-        // função push() posiciona item no array
-        // cont.push(num1)
-        res.innerHTML += `\u{1F600}${i}, `
+    if(intrvl == 0) {
+        alert("Insira um valor válido!")
     }
 
-
-    // função join() transforma array em string
-    // console.log(cont.join(", "))
-    // res.innerHTML = `<p>${cont.join(", ")}</p>`
+    for(let i = num1; i <= num2 ;i += intrvl) {
+        
+        res.innerHTML += `\u{1F600}${i}, `
+    }
     
 }
